c6d5f658c917de8950e02a035cb3c3859d3917ea
Kseniia Kozhukhova <seniurbano@gmail.com>
2018-05-09 19:53:38 +0300

    Create data service and import it to question list component

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Question } from '../models/Question';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  questions:Question[];
+  constructor() { 
+    this.questions = [
+      {
+        text: 'What is your name?',
+        answer: 'My name is Jonathon',
+        hide: true
+      },
+      {
+        text: 'What is your favorite color?',
+        answer: 'My favorite color is violet ',
+        hide: true
+      },
+      {
+        text: 'What is your favorite language?',
+        answer: 'My favorite language is Javascript',
+        hide: true
+      }
+    ];
+  }
+
+  getQuestions() {
+    return this.questions;
+  }
+}
