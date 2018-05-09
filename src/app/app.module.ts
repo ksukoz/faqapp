7cdc7cee32d802be7050d6ccf47c2f0abfa5813c
Kseniia Kozhukhova <seniurbano@gmail.com>
2018-05-09 18:44:33 +0300

    Create question list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { QuestionListComponent } from './components/question-list/question-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    QuestionListComponent
   ],
   imports: [
     BrowserModule
